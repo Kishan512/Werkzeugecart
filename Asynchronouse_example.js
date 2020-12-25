78f04bf2ee3561e086c2e4c58f125640e30afd85
Kishan512 <76422990+Kishan512@users.noreply.github.com>
2020-12-25 18:47:07 +0530

    Commit

---
+++ Asynchronouse_example.js
@@ -0,0 +1,13 @@
+console.log("this is Asynchronouse Example ");
+
+
+setTimeout(() => {
+
+for (var i = 0; i < i.length; i++) {
+	print("this is index"+i)
+}
+}, 1000);
+
+console.log('done print');
+
+
