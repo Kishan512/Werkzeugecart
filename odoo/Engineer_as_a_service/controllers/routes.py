a030a008d33aa667c60c1fa3f3c35c0b1ec4b641
kishan512 <kishanrathod826@gmail.com>
2021-04-16 15:55:10 +0530

    [WIP] improve

--- odoo/Engineer_as_a_service/controllers/routes.py
+++ odoo/Engineer_as_a_service/controllers/routes.py
@@ -6,6 +6,8 @@ class Main(http.Controller):
 
     @http.route('/home', type="http")
     def mypath(self, **kwargs):
+        request.session.get('user_id') and request.session.pop('user_id')
+        request.session.get('user_name') and request.session.pop('user_name')
         # students = request.env['user.list'].search([])
         return request.render('Engineer_as_a_service.home')
 
@@ -17,15 +19,19 @@ class Main(http.Controller):
     def login_submit(self, **kwargs):
         # print(kwargs['email'])
         res = request.env['user.list'].search([('email', '=', kwargs.get('email')),('password', '=', kwargs.get('password'))])
-        print(res)
         # import pdb; pdb.set_trace()
         if len(res):
+            request.session['user_id'] = res.id
+            request.session['user_name'] = res.name
+            request.session['role'] = res.role
+            print(request.session['role'])
             return request.render('Engineer_as_a_service.home_client')
         else:
             return http.local_redirect('/login')
 
     @http.route('/home_client', type="http")
     def home_client(self, **kwargs):
+        request.session.get('user_id')
         return request.render('Engineer_as_a_service.home_client')
 
 
@@ -40,7 +46,6 @@ class Main(http.Controller):
 
     @http.route('/signup_submit', type="http", method="POST", csrf=False)
     def signup_submit(self, **kwargs):
-        print(kwargs)
         # import pdb; pdb.set_trace()
         request.env['user.list'].create({
             'role': "client",
@@ -54,29 +59,57 @@ class Main(http.Controller):
             })
         return http.local_redirect('/login')
 
-    @http.route('/delete/<model("user.list"):std>', type="http")
-    def delete(self, std=None, **kwargs):
-        # print(std)    
+    @http.route('/engineer_signup_submit', type="http", method="POST", csrf=False)
+    def engineer_signup_submit(self, **kwargs):
+        # print(kwargs)
         # import pdb; pdb.set_trace()
-        std.unlink()
-        return http.local_redirect('/mypath')
+        request.env['user.list'].create({
+            'role': "engineer",
+            'email': kwargs.get("email"),
+            'name': kwargs.get("fname"),
+            'password': kwargs.get("password"),
+            'address': kwargs.get("address"),
+            'mobile_no': kwargs.get("mobno"),
+            'specialist': kwargs.get("specialist"),
+            'experience': kwargs.get("experience"),
+            })
+        return http.local_redirect('/login')
+
+    # @http.route('/delete/<model("user.list"):std>', type="http")
+    # def delete(self, std=None, **kwargs):
+    #     # print(std)    
+    #     # import pdb; pdb.set_trace()
+    #     std.unlink()
+    #     return http.local_redirect('/mypath')
 
     @http.route('/client_Engineer_list', type="http")
     def client_Engineer_list(self, **kwargs):
-        engineer_list = request.env['user.list'].search([('role', '=', 'client')])
+        engineer_list = request.env['user.list'].search([('role', '=', 'engineer')])
         return request.render('Engineer_as_a_service.client_Engineer_list',{'engineer_list' : engineer_list})
         
     @http.route('/view_engineer_deatail/<int:engineer_id>', type="http")
     def view_engineer_deatail(self,engineer_id, **kwargs):
-        print(engineer_id)
         engineer_list = request.env['user.list'].browse(engineer_id)
         return request.render('Engineer_as_a_service.view_engineer_detail',{'engineer_list' : engineer_list})
-        
+    
+    @http.route('/book_engineer/<int:engineer_id>/<int:client_id>', type="http")
+    def book_engineer(self,engineer_id,client_id, **kwargs):
+        request.env['orders'].create({
+            'engineer_id' : engineer_id,
+            'client_id' : client_id
+        })  
+        return request.render('Engineer_as_a_service.home_client')
+
+
     
     @http.route('/orders', type="http")
     def orders(self, **kwargs):
-        return request.render('Engineer_as_a_service.orders')
+        order_list = request.env['orders'].search([('client_id', '=', request.session.get('user_id'))])
+        return request.render('Engineer_as_a_service.orders',{'order_list':order_list})
         
     @http.route('/client_profile', type="http")
     def client_profile(self, **kwargs):
         return request.render('Engineer_as_a_service.client_profile')
+
+
+        
--- odoo/Engineer_as_a_service/models/main.py
+++ odoo/Engineer_as_a_service/models/main.py
@@ -4,15 +4,16 @@ class Userslist(models.Model):
     _name = 'user.list'
     _description = "Engineers and Clients detail"
 
-    role = fields.Char(string="role")
+    # order_id = fields.Many2one('orders', string="orders")
+    role = fields.Selection([('client', 'Client'), ('engineer', 'Engineer')], string="Role")
     email = fields.Char(string="Email id")
     name = fields.Char(string="Name")
     mobile_no = fields.Char(string="mobile_no")
     password = fields.Char(string="password")
-    address = fields.Char(string="Address", translate=True)
+    address = fields.Char(string="Address")
     session = fields.Char(string="session")
-    specialist = fields.Char(string="specialist", translate=True)
-    experience  = fields.Char(string="experience", translate=True)
+    specialist = fields.Char(string="specialist")
+    experience  = fields.Char(string="experience")
     # birthday = fields.Date(string="Birthday", required=True)
     # age = fields.Integer(compute="calculate_age", store=True)
     # gender = fields.Selection([('male', 'Male'), ('female', 'Female')], default="male")
@@ -24,3 +25,14 @@ class Userslist(models.Model):
     # sem_fee = fields.Integer(string="Fee per semester")
     # enrollment_no = fields.Integer(string="Enrollment number")
     # branch = fields.Char(string="Branch")
+
+class orders(models.Model):
+    _name = 'orders'
+    _description = "orders detail"
+
+    # order_id = fields.Integer(string="order_id")
+    engineer_id = fields.Many2one('user.list', string="engineer_id")
+    client_id = fields.Many2one('user.list', string="client_id")
+    # user_id = fields.One2many('user.list', 'id', string="user record")
+
+
